864beaf133b5f122757cd3a9fc1649b33c91d9a2
Vamsi Lingamaneni <44448313+vaamsii@users.noreply.github.com>
2026-08-24 01:32:52 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // int left = 0;
+        // int right = 1;
+        // int maxProfit = 0;
+
+        // while(right < prices.length){
+        //     if(prices[left] < prices[right]){
+        //         int profit = prices[right] - prices[left];
+        //         maxProfit = Math.max(maxProfit, profit);
+        //     }
+        //     else{
+        //         left = right;
+        //     }
+        //     right++;
+        // }
+
+        // return maxProfit;
+
+
+        int minBuy = prices[0];
+        int maxProfit = 0;
+
+        for(int price: prices){
+            maxProfit = Math.max(maxProfit, price - minBuy);
+
+            minBuy = Math.min(minBuy, price);
+        }
+        return maxProfit;
+    }
+}
